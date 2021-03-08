75d5bb2b6e93f13100144aa88c152db1ea9af42b
in-hwan Choi <cjhih43@gmail.com>
2021-03-08 19:04:54 +0900

    fix: lint

--- configs/rollup.config.es.js
+++ configs/rollup.config.es.js
@@ -1,28 +1,28 @@
-import copy from 'rollup-plugin-copy';
-import json from 'rollup-plugin-json';
-import babel from 'rollup-plugin-babel';
+import copy from 'rollup-plugin-copy'
+import json from 'rollup-plugin-json'
+import babel from 'rollup-plugin-babel'
 
 export default {
   name: 'videojsIma',
   input: 'src/ima-plugin.js',
   output: {
     file: 'dist/videojs.ima.es.js',
-    format: 'es',
+    format: 'es'
   },
   external: ['video.js', 'videojs-contrib-ads'],
   globals: {
-    'video.js': 'videojs',
+    'video.js': 'videojs'
   },
   plugins: [
     json(),
     copy({
-      'src/css/videojs.ima.css': 'dist/videojs.ima.css',
+      'src/css/videojs.ima.css': 'dist/videojs.ima.css'
     }),
     copy({
-      'src/css/videojs.ima.css': 'dist/videojs.ima.scss',
+      'src/css/videojs.ima.css': 'dist/videojs.ima.scss'
     }),
     babel({
-      exclude: 'node_modules/**',
-    }),
-  ],
-};
+      exclude: 'node_modules/**'
+    })
+  ]
+}
